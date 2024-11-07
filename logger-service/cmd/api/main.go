c2dd16e60e1322c6f00a14f35e3261412a45a7fe
Gabriel José <gabriel.josemelo@hotmail.com>
2024-11-07 10:59:06 -0300

    add rpc server to logger service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -45,6 +47,9 @@ func main() {
 		Models: data.New(mongoClient),
 	}
 
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	app.serve()
 }
 
@@ -59,6 +64,26 @@ func (app *Config) serve() {
 
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting rpc on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+
+	if err != nil {
+		return err
+	}
+
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
---
+++ logger-service/cmd/api/rpc.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log-service/data"
+	"time"
+)
+
+type RPCServer struct{}
+
+type RPCPayload struct {
+	Name string
+	Data string
+}
+
+func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
+	collection := mongoClient.Database("logs").Collection("logs")
+
+	_, err := collection.InsertOne(context.TODO(), data.LogEntry{
+		Name:      payload.Name,
+		Data:      payload.Data,
+		CreatedAt: time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Mongo Error: ", err)
+		return err
+	}
+
+	*resp = "Processed payload via RPC: " + payload.Name
+
+	return nil
+}
