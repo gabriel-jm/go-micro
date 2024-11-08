429aa53f36b7f26836ca0fede0f008d03ce8f727
Gabriel José <gabriel.josemelo@hotmail.com>
2024-11-08 11:26:48 -0300

    add grpc to logger service

---
+++ logger-service/cmd/api/grpc.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log-service/data"
+	"log-service/logs"
+)
+
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
+
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	err := l.Models.LogEntry.Insert(logEntry)
+
+	if err != nil {
+		response := &logs.LogResponse{Result: "failed"}
+
+		return response, err
+	}
+
+	response := &logs.LogResponse{
+		Result: "logged",
+	}
+
+	return response, nil
+}
